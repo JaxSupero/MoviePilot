3edc940a5bd6755f3a8f26eef54d65a01960dc12
Jax Supero <wingsxxx@foxmail.com>
2026-03-29 22:31:52 +0800

    Update __init__.py

--- plugins/unifiedpushnotifier/__init__.py
+++ plugins/unifiedpushnotifier/__init__.py
@@ -16,7 +16,7 @@ class UnifiedPushNotifier(_PluginBase):
     plugin_name = "统一推送服务"
     plugin_desc = "将MoviePilot事件推送到自定义的统一推送服务（支持标题/内容模板、Token认证）"
     plugin_icon = "webhook.png
-    plugin_version = "1.1"
+    plugin_version = "0.1"
     plugin_author = "WINGS"
     author_url = "https://github.com/yourname"
     plugin_config_prefix = "webhook_push_"
